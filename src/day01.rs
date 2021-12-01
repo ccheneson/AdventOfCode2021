6af6ec989d285c69eaf7c204dba4c946f9648b76
Cyril Cheneson <ccheneson@gmail.com>
2021-12-01 18:16:01 +0100

    day01 - aoc

---
+++ src/day01.rs
@@ -0,0 +1,88 @@
+
+use std::{collections::VecDeque, iter::Sum};
+
+use anyhow::Result;
+
+pub fn day01() -> Result<()> {
+    day01_part01()?;
+    day01_part02()?;
+    Ok(())
+}
+
+pub fn day01_part01() -> Result<()> {
+    let mut file = include_str!("../input/day01/input01.txt").lines();
+    let mut previous = file.next().unwrap().parse::<i32>()?;
+    let mut larger_count = 0;
+
+    for line in file {
+        let line = line.parse::<i32>()?;
+        if line > previous {
+            larger_count += 1;
+        }
+        previous = line;
+    }
+
+    println!("How many measurements are larger than the previous measurement - part1 ? {}", larger_count);
+
+    Ok(())
+}
+
+
+
+
+pub fn day01_part02() -> Result<()> {
+    let file = include_str!("../input/day01/input01.txt").lines();
+
+    
+    let mut queue = FixSizeQueue::<i32, {3 as usize}>::new();
+    let mut previous: Option<i32> = None;
+    let mut larger_count = 0;
+
+    for line in file {
+        let line = line.parse::<i32>()?;
+        queue.push(line);
+        if let Some(sum) = queue.sum() {
+            match previous {
+                None => previous = queue.sum(), //N/A No previous sum - no increase of counter
+                Some(value) if value < sum => {
+                    larger_count += 1;
+                    previous = Some(sum);
+                },
+                _ => previous = Some(sum)
+            }            
+        }        
+    }
+
+    println!("How many measurements are larger than the previous measurement - part2 ? {}", larger_count);
+
+    Ok(())
+}
+
+struct FixSizeQueue<T, const N: usize> {
+    queue : VecDeque<T>
+}
+
+impl<T: Sum + Copy, const N: usize> FixSizeQueue<T, N>{
+
+    fn push(&mut self, value: T)  {
+        if self.queue.len() >= N {
+            self.queue.pop_back();
+        }
+        self.queue.push_front(value);        
+    }
+
+    fn sum(&self) -> Option<T> {
+        if self.queue.len() >= N {
+            Some(self.queue.iter().copied().sum())
+        } else {
+            None
+        }
+    }
+
+    fn new() -> Self {
+        Self {
+            queue : VecDeque::with_capacity(N)
+        }
+    }
+}
+
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod day01;
+
+use anyhow::Result;
+use day01::day01;
+
+
+fn main() -> Result<()> {
+    day01()?;
+    Ok(())
+}
