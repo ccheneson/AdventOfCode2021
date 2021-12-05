33d12666d33993021d59a8e88d8713f015cbfb25
Cyril Cheneson <ccheneson@gmail.com>
2021-12-05 13:25:59 +0100

    [day05]part1

---
+++ src/day05.rs
@@ -0,0 +1,141 @@
+use anyhow::Result;
+
+pub fn run() -> Result<()> {
+    part01::run()?;
+    part02::run()?;
+    Ok(())
+}
+
+#[derive(Debug, PartialEq , Eq, Hash)]
+struct Coordinates {
+    x: u16,
+    y: u16
+}
+
+impl Coordinates {
+    fn from_slice(s: &[u16]) -> Self {
+        Self { x: s[0], y : s[1] }
+    }
+}
+
+#[derive(Debug)]
+struct Lines {
+    start: Coordinates,
+    end: Coordinates
+}
+
+#[derive(Debug)]
+enum CoordinateBase {
+    X(u16), Y(u16)
+}
+
+impl Lines {
+
+    fn generate_coordinates(&self, base: CoordinateBase, from_incl: u16, to_incl: u16) -> Vec<Coordinates> {
+        let up = self.start.y <  self.end.y;
+        //The below is needed for .rev()
+        let (from_incl, to_incl) = if from_incl < to_incl { (from_incl, to_incl) } else { (to_incl, from_incl) };
+        match base {
+            CoordinateBase::X(value) => {
+                if up { 
+                    (from_incl..=to_incl).into_iter().map(|p| Coordinates { x: value, y: p }).collect()
+                } else {
+                    (from_incl..=to_incl).rev().into_iter().map(|p| Coordinates { x: value, y: p }).collect()
+                }
+            },
+            CoordinateBase::Y(value) => {
+                if up { 
+                    (from_incl..=to_incl).into_iter().map(|p| Coordinates { x: p, y:value }).collect()
+                } else {
+                    (from_incl..=to_incl).rev().into_iter().map(|p| Coordinates { x: p, y:value }).collect()
+                }
+            }
+        }
+    }
+
+    fn points_covered(&self) -> Vec<Coordinates> {
+        if self.start.x == self.end.x {
+            let base = self.start.x;
+            self.generate_coordinates(CoordinateBase::X(base), self.start.y, self.end.y)
+        } else if self.start.y == self.end.y {
+            let base = self.start.y;
+            self.generate_coordinates(CoordinateBase::Y(base), self.start.x, self.end.x)
+        } else  {
+            vec!()
+        }
+    }
+}
+
+mod part01 {
+    use anyhow::Result;
+    use itertools::Itertools;
+
+    use crate::day05::{Coordinates, Lines};
+
+    fn parse_line_to_coordinates(line: &str) -> Lines  {
+        let coordinates   = line
+                                    .split("->")
+                                    .map(|e|e.trim())
+                                    .map(|e| {
+                                        e.split(",")
+                                            .map(|e| e.parse::<u16>().unwrap())
+                                            .collect::<Vec<u16>>()
+                                    })
+                                    .collect::<Vec<Vec<u16>>>();
+
+        let start = &coordinates[0];
+        let end = &coordinates[1];    
+        Lines { start: Coordinates::from_slice(start) , end: Coordinates::from_slice(end)  }        
+    }
+
+    pub fn run() -> Result<()> {
+        let file = include_str!("../input/day05.txt");
+        let lines: Vec<Lines> = file.lines().map(|line| parse_line_to_coordinates(line)).collect();
+        let covered_points: Vec<Coordinates> = 
+            lines.into_iter().flat_map(|e| e.points_covered()).collect();
+
+        //Get duplicates - if duplicates, it means there is at least 2 occurences
+        let total = covered_points.iter().duplicates().count();        
+
+        println!("At how many points do at least two lines overlap? {}", total);
+        
+        Ok(())
+    }
+}
+
+mod part02 {
+    use anyhow::Result;
+
+    pub fn run() -> Result<()> {
+        Ok(())
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::{Coordinates, Lines};
+
+
+
+    #[test]
+    fn test_entry_point() {
+        let line1 = Lines {
+            start:  Coordinates { x:1 , y: 1 },
+            end:    Coordinates { x:1 , y: 3 }
+        };
+        let line2 = Lines {
+            start:  Coordinates { x:9 , y: 7 },
+            end:    Coordinates { x:7 , y: 7 }
+        };
+        let line3 = Lines {
+            start:  Coordinates { x:3 , y: 8 },
+            end:    Coordinates { x:5 , y: 9 }
+        };
+
+        assert_eq!(line1.points_covered(), vec!(Coordinates { x:1, y: 1}, Coordinates { x:1, y: 2}, Coordinates { x:1, y: 3}));
+        assert_eq!(line2.points_covered(), vec!(Coordinates { x:9, y: 7}, Coordinates { x:8, y: 7}, Coordinates { x:7, y: 7}));
+        assert_eq!(line3.points_covered(), vec!());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 use anyhow::Result;
 
 
 fn main() -> Result<()> {
-    day01::run()?;
-    day02::run()?;
-    day03::run()?;
-    day04::run()?;
+    //day01::run()?;
+    //day02::run()?;
+    //day03::run()?;
+    //day04::run()?;
+    day05::run()?;
     Ok(())
 }
