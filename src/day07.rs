0876a3e02829d007a925336ccb683b9d692ec253
Cyril Cheneson <ccheneson@gmail.com>
2021-12-07 16:09:40 +0100

    [day07]optimised version

--- src/day07.rs
+++ src/day07.rs
@@ -4,7 +4,8 @@ pub fn run() -> Result<()> {
     let file = include_str!("../input/day07.txt");
 
     part01::run(file)?;
-    part02::run(file)?;
+    //part02::run(file)?;
+    part02::run_bis(file)?;
 
     Ok(())
 }
@@ -48,10 +49,12 @@ mod part01 {
 }
 
 mod part02 {
+    use std::collections::HashMap;
+
     use anyhow::Result;
     use super::Position;
 
-    
+    #[allow(unused)]
     pub fn run(file: &str) -> Result<i32> {        
         let positions : Vec<Position>   = file.lines()
                 .flat_map(|line| line.split(",").collect::<Vec<&str>>())
@@ -79,6 +82,42 @@ mod part02 {
         let min = consummed_fuels.into_iter().min().unwrap();
         println!("How much fuel must they spend to align to that position? part 2 (slow from build, but fast with --release) {}", min);
         
+        Ok(min)
+    }
+
+    pub fn run_bis(file: &str) -> Result<u32> {        
+        let positions : Vec<Position>   = file.lines()
+                .flat_map(|line| line.split(",").collect::<Vec<&str>>())
+                .map(|e| e.trim().parse::<u16>().unwrap())
+                .map(|e| Position(e))
+                .collect();
+                
+
+        let mut consummed_fuels: Vec<u32> = vec!();
+
+        let max_position = positions.iter().max_by_key(|p| p.position()).unwrap().position();
+
+        //Build hashmap of all possible position and consummption
+        let bases : HashMap<usize, u32> = (0..=max_position).fold(vec!((0,0)), |mut acc, elem| {       
+            let previous_total = *(acc.last().unwrap());                        
+            acc.push((elem as usize, (previous_total.1 + elem as u32)));
+            acc
+        }).into_iter().collect();
+
+        for target_position in 0..=max_position {
+            let moves = positions
+                .iter()
+                .map(|position| {
+                    let count_moves = (target_position as i32 - position.position() as i32).abs() as usize;                                    
+                    bases[&(count_moves as usize)]
+                }
+            );
+            consummed_fuels.push(moves.sum());
+        }
+
+        let min = consummed_fuels.into_iter().min().unwrap();
+        println!("How much fuel must they spend to align to that position? part 2 (optimised version with base hashmap) {}", min);
+
         Ok(min)
     }
 }
